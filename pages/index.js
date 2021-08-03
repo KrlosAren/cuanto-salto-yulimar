5836d4667c8332b5f6062e4fd8f7d621d51312e5
Carlos Lopez Z <krlosaren@gmail.com>
2021-08-02 23:18:09 -0400

    [add] cropimg dynamic

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,11 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import { useEffect, useState } from 'react';
-import Yulimar from '../public/img/yulimar.jpg';
+import Djovokic from '../public/img/djokovik.svg';
+import Messi from '../public/img/messi.svg';
+import Phells from '../public/img/phellls.svg';
+import Simone from '../public/img/simone.svg';
+import Yulimar from '../public/img/yulimar.svg';
 
 const selected = {
   atletas: [
@@ -9,13 +13,13 @@ const selected = {
       id: 1,
       name: 'leonel messi',
       height: 1.7,
-      image: Yulimar,
+      image: Messi,
     },
     {
       id: 2,
       name: 'simone biles',
       height: 1.42,
-      image: Yulimar,
+      image: Simone,
     },
     {
       id: 3,
@@ -27,13 +31,19 @@ const selected = {
       id: 4,
       name: 'djokovic',
       height: 1.88,
-      image: Yulimar,
+      image: Djovokic,
     },
     {
       id: 5,
       name: 'yulimar',
       height: 1.93,
       image: Yulimar,
+    },
+    {
+      id: 6,
+      name: 'Phells',
+      height: 1.89,
+      image: Phells,
     },
   ],
 };
@@ -84,6 +94,11 @@ export default function Home() {
     }
   }, [numberPhoto]);
 
+  //   <h1>
+  //   Cuanto Salto Yulimar Rojas : {total ? total.toFixed(2) : 15.67} -{' '}
+  //   {option ? `${option.name}s` : 'm'}
+  // </h1>
+
   return (
     <>
       <div>
@@ -96,162 +111,164 @@ export default function Home() {
         </Head>
 
         <main>
-          <h1>
-            Cuanto Salto Yulimar Rojas : {total ? total.toFixed(2) : 15.67} -{' '}
-            {option ? `${option.name}s` : 'm'}
-          </h1>
+          <header>
+            <p>Juegos Olímpicos - Tokio 2020</p>
+            <h1>Yulimar Rojas</h1>
+          </header>
+          <div className='box-line' style={{ backgroundColor: '#F3B23A' }} />
+          <div className='box-line' style={{ backgroundColor: '#3A9AF3' }} />
+          <div className='box-line' style={{ backgroundColor: '#F33A3A' }} />
+          <article>
+            <p>TRIPLE SALTO</p>
+          </article>
+          <section>
+            <p>Yulimar saltó el equivalente a...</p>
+            <input type='text' list='data' onChange={handleChange} />
+            <datalist id='data'>
+              {atletas.map((item) => (
+                <option key={item.id} value={item.name} />
+              ))}
+            </datalist>
+          </section>
           <div className='img-container'>
             {option &&
               numberPhoto.map((i, index) => (
-                <div key={index} id={`${index}-img`}>
+                <div key={index} id={`${index}-img`} className=''>
                   <Image src={option.image} alt={option.name} />
                 </div>
               ))}
           </div>
-          <p>Record Olimpico y Mundial</p> <h3>15.67m</h3>
-          <input type='text' list='data' onChange={handleChange} />
-          <datalist id='data'>
-            {atletas.map((item) => (
-              <option key={item.id} value={item.name} />
-            ))}
-          </datalist>
+          <div className='total-container'>
+            <div>
+              <p>
+                <strong>{total ? total.toFixed(2) : '15.67 m'}</strong>
+              </p>
+              <p>{option ? option.name : 'Salto Yulimar'}</p>
+            </div>
+          </div>
+          <footer>
+            <div>Diseñado y desarrollado por @ansilliano @krlosaren</div>
+            <div>¿¿RRSS de Yulimar acá??</div>
+          </footer>
         </main>
       </div>
 
       <style jsx>{`
+        main {
+          width: 100%;
+          height: auto;
+          display: grid;
+          grid-template-columns: repeat(12, 1fr);
+        }
+
+        header {
+          grid-column: 1/-1;
+          grid-row: 1/2;
+          width: 100%;
+          padding: 10px 18px;
+          height: 120px;
+        }
+
+        header > p {
+          margin: 0;
+          color: #29140f;
+          text-align: right;
+          text-decoration: underline;
+        }
+
+        header > h1 {
+          margin: 0;
+          font-weight: 400;
+          font-size: 60px;
+          color: #29140f;
+          text-align: right;
+        }
+
+        .box-line {
+          grid-column: 1/-1;
+          height: 2px;
+        }
+
+        article {
+          grid-column: 1/-1;
+          height: 100%;
+          background-color: #8f302f;
+          display: flex;
+          align-items: center;
+          justify-content: flex-end;
+        }
+
+        article > p {
+          margin: 0;
+          padding: 0;
+          font-size: 100px;
+          font-weight: 700;
+          margin-right: 45px;
+          color: #ffffff;
+        }
+
+        section {
+          grid-column: 1/-1;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          flex-direction: column;
+        }
+
+        section > p {
+          color: #29140f;
+          font-size: 30px;
+          font-weight: 400;
+        }
+
         .img-container {
           width: 100%;
           display: grid;
+          grid-column: 2/-2;
           grid-template-columns: repeat(${gridColumns}, 1fr);
+          gap: 20px;
+          min-height: 200px;
+          height: 100%;
         }
 
         .img-crop {
           border-radius: 10px;
           clip-path: inset(0 ${img} 0 0);
         }
-      `}</style>
 
-      <style jsx global>{`
-        .container {
-          min-height: 100vh;
-          padding: 0 0.5rem;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-          height: 100vh;
+        .total-container {
+          grid-column: 5/-6;
+          margin-top: 10px;
+          margin-bottom: 10px;
         }
 
-        .main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        .footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        .footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          flex-grow: 1;
-        }
-
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
-        }
-
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
-        }
-
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
-        }
-
-        .title,
-        .description {
+        .total-container > div {
           text-align: center;
-        }
-
-        .description {
-          line-height: 1.5;
-          font-size: 1.5rem;
-        }
-
-        .code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-
-        .grid {
           display: flex;
-          align-items: center;
           justify-content: center;
-          flex-wrap: wrap;
-          max-width: 800px;
-          margin-top: 3rem;
+          align-items: center;
+          border: 1px solid #29140f;
+          width: 500px;
+          height: 40px;
+          background-color: #efdedd;
+          border-radius: 5px;
         }
 
-        .card {
-          margin: 1rem;
-          padding: 1.5rem;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-          border: 1px solid #eaeaea;
-          border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
-          width: 45%;
-        }
-
-        .card:hover,
-        .card:focus,
-        .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
-        }
-
-        .card h2 {
-          margin: 0 0 1rem 0;
-          font-size: 1.5rem;
-        }
-
-        .card p {
+        .total-container > div > p {
+          padding: 0 10px;
           margin: 0;
-          font-size: 1.25rem;
-          line-height: 1.5;
         }
 
-        .logo {
-          height: 1em;
-          margin-left: 0.5rem;
-        }
-
-        @media (max-width: 600px) {
-          .grid {
-            width: 100%;
-            flex-direction: column;
-          }
+        footer {
+          width: 100%;
+          height: 40px;
+          grid-column: 1/-1;
+          padding: 10px;
+          color: #ffff;
+          background-color: #29140f;
+          display: flex;
+          justify-content: space-between;
+          align-items: center;
         }
       `}</style>
     </>
